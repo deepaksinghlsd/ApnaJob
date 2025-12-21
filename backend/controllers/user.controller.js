95534e182dc4ba0f5107e78172d170d0470974e4
Deepak <ds9523717267@gmail.com>
2025-12-21 13:50:09 +0530

    Working on these project to ai integrate on these project

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -7,8 +7,6 @@ import cloudinary from "../utils/cloudinary.js";
 export const register = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, password, role } = req.body;
-    console.log(req.body);
-
     if (!fullname || !email || !phoneNumber || !password || !role) {
       return res.status(400).json({
         message: "Something is missing",
